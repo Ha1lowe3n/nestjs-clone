1de32f47f70c4818c7aa2751a9882aa2d5dd18d5
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-03-26 17:32:18 +0800

    create HttpError, add exception filter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
 import { Server } from "http";
+
+import { ExceptionFilter } from "./errors/exception.filter";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
 
@@ -9,21 +11,32 @@ export class App {
     server: Server;
     logger: LoggerService;
     userController: UserController;
+    exceptionFilter: ExceptionFilter;
 
-    constructor(logger: LoggerService, userController: UserController) {
+    constructor(
+        logger: LoggerService,
+        userController: UserController,
+        exceptionFilter: ExceptionFilter
+    ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
         this.userController = userController;
+        this.exceptionFilter = exceptionFilter;
     }
 
     useRoutes() {
         this.app.use("/users", this.userController.router);
     }
 
+    useExceptionFilters() {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+    }
+
     /** initial app */
     public async init() {
         this.useRoutes();
+        this.useExceptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`Сервер запущен на порту ${this.port}`);
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 import { BaseController } from "../common/base.controller";
+import { HttpError } from "../errors/http-error";
 import { LoggerService } from "../logger/logger.service";
 
 export class UserController extends BaseController {
@@ -17,6 +18,7 @@ export class UserController extends BaseController {
     }
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok<string>(res, "login");
+        //this.ok<string>(res, "login");
+        next(new HttpError(401, "Ошибка авторизации", "[login]"));
     }
 }
