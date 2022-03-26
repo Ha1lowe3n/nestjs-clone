e364cb9329d0d703accb9f816fcc2926d6ceb7de
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-03-26 14:55:42 +0800

    add logger in app with simple DI

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
     app: Express;
     port: number;
     server: Server;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     useRoutes() {
         // this.app.use("/users");
     }
 
-    // запускаем наше приложение
+    /** initial app */
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log("Сервер запущен");
+        this.logger.log(`Сервер запущен на порту ${this.port}`);
     }
 }
